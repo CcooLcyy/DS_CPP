2581c463e0665d9468a4ba1651af114306213e03
Dannys <mutex_@outlook.com>
2023-12-31 17:05:19 +0800

    完善单链表的基本声明

--- include/Linked_list.h
+++ include/Linked_list.h
@@ -7,19 +7,29 @@ namespace Linked_list {
 
 struct Node {
     using node_ptr = std::shared_ptr<Node>;
+    using value_type = int;
+
     Node() : Node(0, nullptr) {}
     Node(int value, Node* next) : value_(value), next_(next) {}
 
-    int value_;
-    std::shared_ptr<Node> next_;
+    value_type value_;
+    node_ptr next_;
 };  //struct Node
 
 class Linked_list {
 public:
+    using value_type = Node::value_type;
+    using node_ptr = Node::node_ptr;
+
     Linked_list() : node(std::make_shared<Node>()) {}
-    bool empty();
+    bool empty() const;
+    Linked_list& insert(int position, value_type insert_value);
+    Linked_list& remove(int position);
+    Linked_list& modify(int position, value_type modified_value);
+    Linked_list& check(int position) const;
+    Linked_list& erase();
 private:
-    Node::node_ptr node;
+    node_ptr node;
     int size = 0;
 };  //class Linked_list
 
