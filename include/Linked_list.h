cfe01322bfd975dc8243cfa01b43c7d3a08cd0dc
Dannys <mutex_@outlook.com>
2023-12-31 17:55:26 +0800

    声明辅助函数： 1. 位置是否合法。 2. 查找节点指针。

--- include/Linked_list.h
+++ include/Linked_list.h
@@ -11,6 +11,8 @@ struct Node {
 
     Node() : Node(0, nullptr) {}
     Node(int value, Node* next) : value_(value), next_(next) {}
+    Node(const Node&) = default;
+    Node& operator=(const Node&) = default;
 
     value_type value_;
     node_ptr next_;
@@ -20,17 +22,20 @@ class List {
 public:
     using value_type = Node::value_type;
     using node_ptr = Node::node_ptr;
+    using pos_type = int;
 
-    List() : head(std::make_shared<Node>()) {}
-    bool empty() const {return !node->value_;}
-    List& insert(int position, value_type insert_value);
-    List& remove(int position);
-    List& modify(int position, value_type modified_value);
-    List& check(int position) const;
+    List() : head_(std::make_shared<Node>()) {}
+    bool empty() const {return !node_->value_;}
+    List& insert(pos_type pos, value_type insert_value);
+    List& remove(pos_type pos);
+    List& modify(pos_type pos, value_type modified_value);
+    List& check(pos_type pos) const;
     List& erase();
 private:
-    node_ptr head;
-    node_ptr node;
+    node_ptr head_;
+    node_ptr node_;
+    bool vaild_pos(pos_type pos) const {return pos <= head_->value_;}
+    List& find_node(pos_type pos) const;
 };  //class Linked_list
 
 }   //namespace Linked_list
